ee65b905e8dc0cd8edb7fe63ddeb391993b42e8b
Colton Arthur Allen <allen.colton@hotmail.com>
2025-12-05 16:17:59 -0800

    day 5 part 2 2025

--- 2025/05/index.js
+++ 2025/05/index.js
@@ -24,4 +24,41 @@ function checkIfIdInRange(id, ranges) {
   return ranges.filter(range =>range[0] <= id && range[1] >= id).length > 0;
 }
 
-partOne(input);
+function partTwo() {
+    let [ranges, ids] = input.split('\n\n');
+    ranges = ranges.split('\n').map(range => range.split('-').map(val => +val));
+    ids = ids.split('\n').map(id => +id);
+    
+    const sortedRanges = ranges.sort((a, b) => a[0] - b[0]);
+
+    let currentMerges = 0;
+
+    do {
+        currentMerges = 0;
+        
+        for (let i = 1; i < sortedRanges.length; i++) {
+            const [prevStart, prevEnd] = sortedRanges[i - 1 - currentMerges];
+            const [currStart, currEnd] = sortedRanges[i - currentMerges];
+            
+            if (currStart <= prevEnd) {
+
+                sortedRanges[i - 1 - currentMerges][1] = Math.max(prevEnd, currEnd);
+
+                sortedRanges.splice(i - currentMerges, 1);
+                currentMerges++;
+                i--;
+            }
+        }
+    } while (currentMerges > 0);
+
+    // count numbers in ranges
+    let totalCount = 0;
+    for (const [start, end] of sortedRanges) {
+        totalCount += (end - start + 1);
+    }
+
+    console.log("Total Count:", totalCount);
+}
+
+// partOne(input);
+partTwo(input);
