7f126774f7b4cf8d7398e897fc87d0a08034309a
Colton Arthur Allen <allen.colton@hotmail.com>
2025-12-06 14:39:33 -0800

    day 6 part 2 2025

--- 2025/06/index.js
+++ 2025/06/index.js
@@ -26,11 +26,42 @@ function partOne() {
 }
 
 function performMathOperations(operator, ...vals) {
-    if (operator === '*') {
-        return vals.reduce((acc, val) => acc * +val, 1);
-    } else if (operator === '+') {
-        return vals.reduce((acc, val) => acc + +val, 0);
-    }
+    return vals.reduce((acc, val) => operator === '*' ? acc * +val : acc + +val, operator === '*' ? 1 : 0);
 }
 
-partOne(input);
+function partTwo() {
+    const rows = input.split('\n');
+
+    const rowCount = rows.length;
+    const colCount = rows[0].length;
+
+    let lastOperator = '';
+    const operatorRow = rows[rowCount - 1].split('').map((val, i) => {
+        if (val === '*' || val === '+') lastOperator = val;
+        return val === ' ' ? lastOperator : val;
+    }).join('');
+
+    let calculations = [];
+    let index = 0;
+
+    for (let col = colCount - 1; col >= 0; col--) {
+        const value = rows.slice(0, -1).map(row => row[col]).join('');
+        const operator = operatorRow[col];
+
+        if (value.trim() === '') {
+            index++;
+            continue;
+        } else {
+            if (!calculations[index]) {
+                calculations.push(+value);
+            } else {
+                calculations[index] = performMathOperations(operator, calculations[index], +value);
+            }
+        }
+    }
+    const result = calculations.reduce((acc, val) => acc + val, 0);
+    console.log("Result:", result);
+}
+
+// partOne();
+partTwo();
